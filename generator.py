c1657f2d0ba80f75c7ff278a2c8e04d1d84ac53d
Om Saran <omsarankre@gmail.com>
2022-04-06 20:06:08 -0500

    Add file range support

--- generator.py
+++ generator.py
@@ -21,6 +21,8 @@ def get_parser():
     parser.add_argument('-d', '--depth', required=True, type=check_depth, help='Depth of the directory structure', dest='depth')
     parser.add_argument('-b', '--breadth', required=True, help='Number of directories per directory (except leaves)', type=int, dest='breadth')
     parser.add_argument('-n', '--num_files', required=True, help='Number of files per directory', type=int, dest='num_files')
+    parser.add_argument('-l', '--file_size_min', help='Minimum file size in bytes', type=int, dest='file_size_min')
+    parser.add_argument('-m', '--file_size_max', help='Maximum file size in bytes', type=int, dest='file_size_max')
 
     return parser
 
@@ -71,12 +73,15 @@ class DirCreator:
 def main():
     parser = get_parser()
     parsed = parser.parse_args(sys.argv[1:])
+    if parsed.num_files:
+        if not parsed.file_size_max or not parsed.file_size_min:
+            parser.error('--num_files requires --file_size_min and --file_size_max')
 
     debug('Following are the arguments:')
     debug(f'Depth = {parsed.depth}. Breadth = {parsed.breadth}. Num Files = {parsed.num_files}')
 
     dir_creator = DirCreator()
-    dir_creator.create(parsed.depth, parsed.breadth, parsed.num_files, (1024, 2048))
+    dir_creator.create(parsed.depth, parsed.breadth, parsed.num_files, (parsed.file_size_min, parsed.file_size_max))
 
 
 if __name__ == '__main__':
