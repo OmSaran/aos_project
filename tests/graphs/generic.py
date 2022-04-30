6b9150f8d017ac13d486b1a2a5e592820fcebf13
AOS <omsarankre@gmail.com>
2022-04-30 15:58:29 +0000

    Cleanup and add cp

--- tests/graphs/generic.py
+++ tests/graphs/generic.py
@@ -1,13 +1,3 @@
-parameters = {
-    'defaults': {
-        'file_size': 1 << 20,
-        'num_files': 100,
-        'sq_poll': False,
-        'num_buffers': 1,
-        'ring_size': 1024,
-    },
-}
-
 import argparse
 import sys
 import os
@@ -17,8 +7,6 @@ import json
 from copy import deepcopy
 from time import time as orig_time
 
-# NOTE: self._rq_sizes can be modified
-
 DEFAULT_NUM_FILES = 100
 DEFAULT_FILE_SIZE = 1024 * 1024
 BUFFER_SIZES = [4096, 8192]
@@ -26,7 +14,8 @@ BUFFER_SIZES = [4096, 8192]
 
 ORIGINAL_CP_BIN_NAME = 'cp'
 FCP_BIN_NAME = 'fcp'
-DEBUG = True
+
+DEBUG = os.environ.get('DEBUG')
 
 def time():
     return orig_time()
@@ -40,21 +29,6 @@ def debug(*args, **kwargs):
 sys.path.append('../../')
 
 from tests.generator import DirCreator
-
-
-def get_parser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--bin', dest='bin_dir', type=str, required=True, help='Directory where binaries (cp, fcp) can be found')
-    parser.add_argument('-t', dest='target_dir', type=str, required=True, help='Directory to run tests at')
-    parser.add_argument('-n', dest='num_files', type=int, required=False, default=DEFAULT_NUM_FILES, help="The num of files to perform experiments on")
-    parser.add_argument('-s', dest='file_size', type=int, required=False, default=DEFAULT_FILE_SIZE, help="The file size to perform experiments on")
-    parser.add_argument('-k', '--sq-poll', dest='sq_poll', required=False, action='store_true', help='Enable sq poll')
-
-    parser.add_argument('--skip_cleanup', dest='skip_cleanup', default=False, action='store_true')
-    parser.add_argument('--skip_sanity', dest='skip_sanity', default=False, action='store_true')
-    parser.add_argument('--skip_drop', dest='skip_drop', default=False, action='store_true')
-
-    return parser
 
 class GenericGraph():
     def __init__(self, config):
@@ -105,7 +79,6 @@ class GenericGraph():
         return file_size, num_files
 
     def create_required_workloads(self):
-        dircr_params = {'file_size', 'num_files'}
         for val in self._variant_values:
             root_path = self._get_root_path(val)
             self._created_dirs.append(root_path)
@@ -117,28 +90,11 @@ class GenericGraph():
         if os.path.exists(path):
             raise Exception(f'Path {path} already exists')
 
-    def _get_fcp_opts(self, variant_val):
-        opts = ''
-        config = deepcopy(self._config['params_info'])
-        config[self._variant_param]['default'] = variant_val
-
-        for k, v in config.items():
-            if v.get('fcp_flag'):
-                if k == 'sq_poll' and v ['default'] == False:
-                    continue
-                opts += f'{v["fcp_flag"]} {v["default"]} '
-
-        
-        debug(f"The final fcp opts are: {opts}")
-        return opts
-
     def _get_fcp_path(self):
         return os.path.join(self._bin_dir, FCP_BIN_NAME)
     
-    def _get_command_fcp(self, src_path, dst_path, variant_val):
-        bin_path = self._get_fcp_path()
-        command = f'{bin_path} ' + self._get_fcp_opts(variant_val) + f'-r {src_path} {dst_path}'
-        return command.split()
+    def _get_cp_path(self):
+        return os.path.join(self._bin_dir, ORIGINAL_CP_BIN_NAME)
 
     def _drop_cache(self):
         # with open('/proc/sys/vm/drop_caches', 'w') as fh:
@@ -160,6 +116,44 @@ class GenericGraph():
 
         return t2 - t1
 
+    def _get_fcp_opts(self, variant_val):
+        opts = ''
+        config = deepcopy(self._config['params_info'])
+        config[self._variant_param]['default'] = variant_val
+
+        for k, v in config.items():
+            if v.get('fcp_flag'):
+                if k == 'sq_poll' and v ['default'] == False:
+                    continue
+                opts += f'{v["fcp_flag"]} {v["default"]} '
+
+        
+        debug(f"The final fcp opts are: {opts}")
+        return opts
+
+    def _get_cp_opts(self, variant_val):
+        opts = ''
+        config = deepcopy(self._config['params_info'])
+        config[self._variant_param]['default'] = variant_val
+
+        for k, v in config.items():
+            if v.get('cp_flag'):
+                opts += f'{v["cp_flag"]} {v["default"]} '
+
+        
+        debug(f"The final fcp opts are: {opts}")
+        return opts
+
+    def _get_command_fcp(self, src_path, dst_path, variant_val):
+        bin_path = self._get_fcp_path()
+        command = f'{bin_path} ' + self._get_fcp_opts(variant_val) + f'-r {src_path} {dst_path}'
+        return command.split()
+
+    def _get_command_cp(self, src_path, dst_path, variant_val):
+        bin_path = self._get_cp_path()
+        command = f'{bin_path} ' + self._get_cp_opts(variant_val) + f'-r {src_path} {dst_path}'
+        return command.split()
+
     def _run_workload_fcp(self, variant_val):
         src_path = self._get_root_path(variant_val)
         dst_path = self._get_copy_root_path(variant_val)
@@ -172,11 +166,29 @@ class GenericGraph():
         self._results['fcp'][self._variant_param].append(variant_val)
         self._results['fcp']['time'].append(t)
 
+        shutil.rmtree(dst_path)    
+    
+    def _run_workload_cp(self, variant_val):
+        src_path = self._get_root_path(variant_val)
+        dst_path = self._get_copy_root_path(variant_val)
+
+        self._ensure_not_present(dst_path)
+        command = self._get_command_cp(src_path, dst_path, variant_val)
+        
+        t = self._run_cmd(command)
+
+        self._results['cp'][self._variant_param].append(variant_val)
+        self._results['cp']['time'].append(t)
+
         shutil.rmtree(dst_path)
 
     def run_workloads_fcp(self):
         for val in self._variant_values:
             self._run_workload_fcp(val)
+    
+    def run_workloads_cp(self):
+        for val in self._variant_values:
+            self._run_workload_cp(val)
         
     def cleanup(self):
         for direc in self._created_dirs:
@@ -220,7 +232,8 @@ def get_params_info():
         },
         'buffer_size': {
             'default': 4096,
-            'fcp_flag': '-b'
+            'fcp_flag': '-b',
+            'cp_flag': '-b'
         },
         'ring_size': {
             'default': 1024,
@@ -230,8 +243,19 @@ def get_params_info():
     return params
 
 
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--bin', dest='bin_dir', type=str, required=False, help='Override directory where binaries (cp, fcp) can be found')
+    parser.add_argument('-t', dest='target_dir', type=str, required=False, help='Override directory to run tests at')
+    parser.add_argument('-f', dest='config_path', type=str, required=False, default='config.json', help='Config file path')
+
+    return parser
+
 def get_parsed():
-    with open("config.json") as fh:
+    parser = get_parser()
+    parsed = parser.parse_args(sys.argv[1:])
+
+    with open(parsed.config_path) as fh:
         config = json.loads(fh.read())
     params = get_params_info()
 
@@ -239,7 +263,7 @@ def get_parsed():
     for k, v in config.get('invariants', {}).items():
         params[k]['default'] = v
     
-    return {
+    ret = {
         'variant': {
             'param': config['variant']['param'],
             'values': config['variant']['values']
@@ -248,8 +272,18 @@ def get_parsed():
         'name': config['name'],
         'target_dir': config['target_dir'],
         'bin_dir': config['bin_dir'],
-        'skip_cleanup': config.get('skip_cleanup', False)
+        'skip_cleanup': config.get('skip_cleanup', False),
+        'run_cp': config.get('run_cp', False)
     }
+
+    # override with cli args
+    if parsed.bin_dir:
+        ret['bin_dir'] = parsed.bin_dir
+    if parsed.target_dir:
+        ret['target_dir'] = parsed.target_dir
+
+    debug(f'Parsed the following config {json.dumps(ret, indent=2)}')
+    return ret
 
 def main():
     parsed = get_parsed()
@@ -259,9 +293,10 @@ def main():
     graph.setup_wdir()
     graph.create_required_workloads()
     graph.run_workloads_fcp()
+    if parsed['run_cp']:
+        graph.run_workloads_cp()
+
     graph.dump_results()
-    # graph.run_workloads_cp()
-    # graph.dump_results()
 
     if not parsed['skip_cleanup']:
         graph.cleanup()
