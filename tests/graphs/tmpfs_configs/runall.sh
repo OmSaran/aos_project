08cd5da9570f5ddb21ae3d90ebc17cdcfddd6482
om <omsarankre@gmail.com>
2022-05-04 17:55:29 +0000

    new results

--- tests/graphs/tmpfs_configs/runall.sh
+++ tests/graphs/tmpfs_configs/runall.sh
@@ -4,7 +4,11 @@ rm -rf /dev/shm/rerun
 mkdir /dev/shm/rerun
 mkdir op
 
+# INSTRUCTIONS:
+# 1. Add git rootdir to PYTHONPATH
+# 2. Do not forget to modify -r -t and --bin arguments in the command below
+
 for i in `ls *_config.json`
 do
-DEBUG=1 python generic.py -f $i -r ../../new_results -t /dev/shm/rerun > op/stdout_$i.out 2> op/stderr_$i.out
+DEBUG=1 python generic.py -f $i -r /home/cc/aos_project/new_results -t /home/cc/aos_project/tests/wdir --bin /home/cc/aos_project/build > op/stdout_$i.out 2> op/stderr_$i.out
 done
